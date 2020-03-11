00aa259643f48ab2b7288c4bd07237b0a2bdbfc7
Zarsla <izalli@yahoo.com>
2020-03-11 01:24:05 -0400

    Add files via upload

--- app/src/main/java/com/example/zpg/LoginActivity.java
+++ app/src/main/java/com/example/zpg/LoginActivity.java
@@ -1,12 +1,19 @@
-package com.example.zpg;
+package com.example.zarslapg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.zarslapg.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "LoginActivity";
@@ -15,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etPassword;
     private Button btnLogin;
 
-    
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
@@ -23,6 +30,9 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
+        if(ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
         btnLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -38,6 +48,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password){
         Log.i(TAG, "Attempting to login user " + username);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Toast.makeText(LoginActivity.this, "Incorrect Login", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Issue with Attempted Login", e);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Successful Login!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
 
