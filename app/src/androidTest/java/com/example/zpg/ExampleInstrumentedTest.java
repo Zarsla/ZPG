d4a2378d5243aa1e6ddb78d49832ae2d0cde9c4d
Zarsla <izalli@yahoo.com>
2020-03-11 01:21:45 -0400

    Add files via upload

--- app/src/androidTest/java/com/example/zpg/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/zpg/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.zpg;
+package com.example.zarslapg;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.zpg", appContext.getPackageName());
+        assertEquals("com.example.zarslapg", appContext.getPackageName());
     }
 }
